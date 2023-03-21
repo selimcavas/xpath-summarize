5fcb821923735583598688f5580433f41f8f6199
Selim Cavas <selim.cavas@ozu.edu.tr>
2023-03-21 12:41:34 +0300

    added summarizer v1

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+from lxml import html
+import json
+import csv
+
+url = input("Enter the website URL: ")
+
+response = requests.get(url)
+content = response.content
+
+tree = html.fromstring(content)
+
+elements = []
+
+for element in tree.xpath('//*'):
+
+    name = element.tag
+    text = element.text
+    xpath = tree.getroottree().getpath(element)
+    clickable = element.tag in ['a', 'button', 'input']
+
+    elements.append({
+        'name': name,
+        'text': text,
+        'xpath': xpath,
+        'clickable': clickable,
+    })
+
+json_array = json.dumps(elements, indent=4)
+
+with open('elements.json', 'w') as f:
+    f.write(json_array)
+
+with open('elements.csv', 'w', newline='') as f:
+    writer = csv.DictWriter(
+        f, fieldnames=['name', 'text', 'xpath', 'clickable'])
+    writer.writeheader()
+    for element in elements:
+        writer.writerow(element)
